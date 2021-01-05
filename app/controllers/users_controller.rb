18e3d0785f159e27e6f1fa86a4067a0a0f9d4e44
Kyle Swaffield <kyle@swaff.id.au>
2021-01-05 20:05:36 +1100

    users-api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    headers['Access-Control-Allow-Origin'] = '*'
+    render json: User.all
   end
 
   # GET /users/1
