6c88401774e08cc919f531403167eeddd3a43df3
Kyle Swaffield <kyle@swaff.id.au>
2021-01-05 19:05:07 +1100

    Flights-seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_075403) do
+ActiveRecord::Schema.define(version: 2021_01_05_075752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2021_01_05_075403) do
     t.string "to"
     t.string "from"
     t.integer "airplane_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "booking_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
