1087339a9fa495bcf8355992ffc2700cd46147ee
Stacey Lewis <stacey.lewis@rea-group.com>
2021-01-05 18:54:46 +1100

    users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_074350) do
+ActiveRecord::Schema.define(version: 2021_01_05_075403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2021_01_05_074350) do
     t.string "to"
     t.string "from"
     t.integer "airplane_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
